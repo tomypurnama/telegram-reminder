5f6bf9c26668469df767fd17d42890fd9a2862d4
tomypurnama <tomypurnamaputra@gmail.com>
2026-02-23 11:18:55 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,13 @@
 import os
 import asyncio
-from telegram import Update, Bot
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("TOKEN")
 
-# command /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot reminder siap 👍\nContoh: /ingatkan 10 minum air")
 
-# command /ingatkan
 async def ingatkan(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
         menit = int(context.args[0])
@@ -24,15 +22,15 @@ async def ingatkan(update: Update, context: ContextTypes.DEFAULT_TYPE):
     except:
         await update.message.reply_text("Format salah.\nContoh: /ingatkan 10 minum air")
 
-def main():
-    app = ApplicationBuilder().token(TOKEN).build()
+async def main():
+    app = Application.builder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("ingatkan", ingatkan))
 
     print("Bot reminder jalan...")
 
-    app.run_polling()
+    await app.run_polling()
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
