b024a33adbab618f021d00530350f46ced8970f1
tomypurnama <tomypurnamaputra@gmail.com>
2026-02-23 17:54:32 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,10 @@ TOKEN = os.getenv("BOT_TOKEN")
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot hidup 🚀")
 
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
 
-app.run_polling()
+if __name__ == "__main__":
+    main()
